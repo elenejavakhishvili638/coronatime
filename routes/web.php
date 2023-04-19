7dfe7ec58384c15585d28666497bb51c721aa96f
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-19 04:14:28 -0700

    feat: add email verification routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/lang', [LanguageController::class, 'switchLang'])->name('setLanguage');
 
-Route::get('/', [AuthController::class, 'create'])->name('login.create');
+Route::get('/email/verify')->middleware('auth')->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}')->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/', [AuthController::class, 'create'])->name('login');
 Route::post('/', [AuthController::class, 'login'])->name('login.store');
 Route::post('logout', [AuthController::class, 'logout'])->name('login.destroy');
 
 Route::get('register', [RegisterController::class, 'create'])->name('register.create');
 Route::post('register', [RegisterController::class, 'store'])->name('register.store');
 
-Route::get('worldwide-statistics', [StatisticController::class, 'showWorldwide'])->name('worldwide.show');
-Route::get('by-country-statistics', [StatisticController::class, 'showByCountry'])->name('country.show');
+Route::get('worldwide-statistics', [StatisticController::class, 'showWorldwide'])->middleware(['auth'])->name('worldwide.show');
+Route::get('by-country-statistics', [StatisticController::class, 'showByCountry'])->middleware(['auth'])->name('country.show');
