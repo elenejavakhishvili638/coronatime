8f0b972c16f6e79ef783e8a4fe15c5d656fe92b5
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-19 23:41:06 -0700

    feat: handle handle post method to send an email

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EmailRequest;
+use App\Models\User;
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -11,8 +14,21 @@ class PasswordResetController extends Controller
     {
         return view('resetPassword.request');
     }
-    public function storeEmail()
+    public function storeEmail(EmailRequest $request)
     {
+        $attributes = $request->validated();
+
+        $user = User::where('email', $attributes['email'])->first();
+
+        if (!$user) {
+            return back()->withErrors(['email' => trans('error')]);
+        }
+
+        $token = app('auth.password.broker')->createToken($user);
+        $url = url('/reset-password/' . $token . '?email=' . $attributes['email']);
+        $user->notify(new ResetPasswordNotification($url));
+
+        return  view('verifyEmail.confirmEmail');
     }
     public function showReset()
     {
