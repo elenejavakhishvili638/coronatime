66101e31725d4b36745eb17e6bb9fbd95ee9c66e
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-24 02:05:38 -0700

    fix: redirection

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/worldwide-statistics';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
