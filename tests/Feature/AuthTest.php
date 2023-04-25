266fcfcf683c4c648ded4a6325f854d45f7a7d35
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-25 02:02:45 -0700

    feat: add AuthTest

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,5 +19,125 @@ class AuthTest extends TestCase
         $response->assertSuccessful();
         $response->assertSee('Sign up for free');
         $response->assertViewIs('sessions.create');
+    }
+
+    public function test_auth_should_redirect_to_dashboard_after_successful_email_login()
+    {
+        $email = "ako@example.com";
+        $password = "password";
+        $username = "ako";
+
+        User::factory()->create([
+            'email' => $email,
+            'password' => bcrypt($password),
+        ]);
+
+        $response = $this->post('/', [
+            'username' => $email,
+            'password' => $password
+        ]);
+
+        $response->assertRedirect(route('worldwide.show'));
+        $this->assertAuthenticated();
+    }
+
+    public function test_auth_should_redirect_to_dashboard_after_successful_username_login()
+    {
+        $email = "ako@example.com";
+        $username = "ako";
+        $password = "password";
+
+        User::factory()->create([
+            'email' => $email,
+            'username' => $username,
+            'password' => bcrypt($password),
+        ]);
+
+        $response = $this->post('/', [
+            'username' => $username,
+            'password' => $password
+        ]);
+
+        $response->assertRedirect(route('worldwide.show'));
+        $this->assertAuthenticated();
+    }
+
+    public function test_user_can_logout()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->post(route('login.destroy'));
+
+        $this->assertGuest();
+
+        $response->assertRedirect('/');
+    }
+
+    public function test_auth_should_fail_with_incorrect_email()
+    {
+        $email = "elene@example.com";
+        $username = "elene";
+        $password = "password";
+
+        User::factory()->create([
+            'email' => $email,
+            'username' => $username,
+            'password' => bcrypt($password),
+        ]);
+
+        $response = $this->post('/', [
+            'username' => 'email@example.com',
+            'password' => $password
+        ]);
+
+
+        $response->assertSessionHasErrors(['username']);
+
+        $response->assertSessionDoesntHaveErrors(['password']);
+    }
+
+    public function test_auth_should_fail_with_incorrect_username()
+    {
+        $email = "elene@example.com";
+        $username = "elene";
+        $password = "password";
+
+        User::factory()->create([
+            'email' => $email,
+            'username' => $username,
+            'password' => bcrypt($password),
+        ]);
+
+        $response = $this->post('/', [
+            'username' => 'elene123',
+            'password' => $password
+        ]);
+
+        $response->assertSessionHasErrors(['username']);
+
+        $response->assertSessionDoesntHaveErrors(['password']);
+    }
+
+    public function test_auth_should_fail_with_incorrect_password()
+    {
+        $email = "elene@example.com";
+        $password = "password";
+
+        User::factory()->create([
+            'email' => $email,
+            'password' => bcrypt($password),
+        ]);
+
+        $response = $this->post('/', [
+            'username' => $email,
+            'password' => 'wrong-password'
+        ]);
+
+
+        $response->assertSessionHasErrors('email', 'Your provided credential could not be verified');
+
+        $response->assertSessionDoesntHaveErrors(['username']);
     }
 }
