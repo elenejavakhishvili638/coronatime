0a48fc8746c569da2b3ead463553906768224fbe
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-26 04:53:28 -0700

    feat: add sort and search tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -15,10 +15,40 @@ class DashboardTest extends TestCase
      */
     use RefreshDatabase;
 
-    public function test_worldwide_page_is_accessible()
+    protected $covidData1;
+    protected $covidData2;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $user = User::factory()->create();
         $this->actingAs($user);
+
+        $this->covidData1 = CovidData::factory()->create([
+            'name' => [
+                'en' => 'searchterm',
+                'ka' => 'საძიებო ტერმინი',
+            ],
+            'confirmed' => 100,
+            'deaths' => 10,
+            'recovered' => 90,
+        ]);
+
+        $this->covidData2 = CovidData::factory()->create([
+            'name' => [
+                'en' => 'other',
+                'ka' => 'სხვა',
+            ],
+            'confirmed' => 200,
+            'deaths' => 20,
+            'recovered' => 180,
+        ]);
+    }
+
+    public function test_worldwide_page_is_accessible()
+    {
+
         $response = $this->get('worldwide-statistics');
         $response->assertSuccessful();
         $response->assertViewIs('dashboard.worldwide');
@@ -27,11 +57,96 @@ class DashboardTest extends TestCase
 
     public function test_by_country_page_is_accessible()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
+
         $response = $this->get('by-country-statistics');
         $response->assertSuccessful();
         $response->assertViewIs('dashboard.byCountry');
         $response->assertViewHasAll(['sumConfirmed', 'sumDeaths', 'sumRecovered', 'countries']);
+    }
+
+    public function test_search_in_english()
+    {
+
+        $response = $this->withSession(['locale' => 'en'])
+            ->get('/by-country-statistics?search=searchterm');
+
+        $response->assertStatus(200);
+        $response->assertViewHas('countries');
+        $viewData = $response->viewData('countries');
+
+        $this->assertCount(1, $viewData);
+        $this->assertEquals($this->covidData1->id, $viewData->first()->id);
+    }
+
+    public function test_search_in_georgian()
+    {
+
+        $response = $this->withSession(['locale' => 'ka'])
+            ->get('/by-country-statistics?search=საძიებო');
+
+        $response->assertStatus(200);
+        $response->assertViewHas('countries');
+        $viewData = $response->viewData('countries');
+
+        $this->assertCount(1, $viewData);
+        $this->assertEquals($this->covidData1->id, $viewData->first()->id);
+    }
+
+    public function test_sort_confirmed_asc()
+    {
+
+        $response = $this->get('/by-country-statistics?sort_by=confirmed&sort_order=asc');
+
+        $response->assertStatus(200);
+        $viewData = $response->viewData('countries');
+
+        $this->assertNotEmpty($viewData);
+        $this->assertTrue($viewData->count() > 1);
+
+        $prevConfirmed = null;
+        foreach ($viewData as $item) {
+            if (!is_null($prevConfirmed)) {
+                $this->assertGreaterThanOrEqual($prevConfirmed, $item->confirmed);
+            }
+            $prevConfirmed = $item->confirmed;
+        }
+    }
+    public function test_sort_deaths_asc()
+    {
+
+        $response = $this->get('/by-country-statistics?sort_by=deaths&sort_order=asc');
+
+        $response->assertStatus(200);
+        $viewData = $response->viewData('countries');
+
+        $this->assertNotEmpty($viewData);
+        $this->assertTrue($viewData->count() > 1);
+
+        $prevDeaths = null;
+        foreach ($viewData as $item) {
+            if (!is_null($prevDeaths)) {
+                $this->assertGreaterThanOrEqual($prevDeaths, $item->deaths);
+            }
+            $prevDeaths = $item->deaths;
+        }
+    }
+    public function test_sort_recovered_asc()
+    {
+
+        $response = $this->get('/by-country-statistics?sort_by=recovered&sort_order=asc');
+
+        $response->assertStatus(200);
+        $viewData = $response->viewData('countries');
+
+        $this->assertNotEmpty($viewData);
+        $this->assertTrue($viewData->count() > 1);
+
+        $prevRecovered = null;
+        foreach ($viewData as $item) {
+            if (!is_null($prevRecovered)) {
+                $this->assertGreaterThanOrEqual($prevRecovered, $item->recovered);
+            }
+            $prevRecovered = $item->recovered;
+        }
     }
 }
